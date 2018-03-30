9c711b4aff1041cf91a6e928adaec305d240b8f0
marionieto87 <marionieto87@hotmail.com>
2018-03-30 11:16:49 -0500

    con mas fragments

--- app/src/main/java/com/example/carlosnieto/apppagos/Fragments/FragmentListaClientes.java
+++ app/src/main/java/com/example/carlosnieto/apppagos/Fragments/FragmentListaClientes.java
@@ -13,12 +13,12 @@ import com.example.carlosnieto.apppagos.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link FragmentRegistraClienteNuevo.OnFragmentInteractionListener} interface
+ * {@link FragmentListaClientes.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link FragmentRegistraClienteNuevo#newInstance} factory method to
+ * Use the {@link FragmentListaClientes#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentRegistraClienteNuevo extends Fragment {
+public class FragmentListaClientes extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class FragmentRegistraClienteNuevo extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public FragmentRegistraClienteNuevo() {
+    public FragmentListaClientes() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class FragmentRegistraClienteNuevo extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentRegistraClienteNuevo.
+     * @return A new instance of fragment FragmentListaClientes.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentRegistraClienteNuevo newInstance(String param1, String param2) {
-        FragmentRegistraClienteNuevo fragment = new FragmentRegistraClienteNuevo();
+    public static FragmentListaClientes newInstance(String param1, String param2) {
+        FragmentListaClientes fragment = new FragmentListaClientes();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +65,7 @@ public class FragmentRegistraClienteNuevo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registra_cliente_nuevo, container, false);
+        return inflater.inflate(R.layout.fragment_lista_clientes, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/carlosnieto/apppagos/MenuActivity.java
+++ app/src/main/java/com/example/carlosnieto/apppagos/MenuActivity.java
@@ -1,46 +1,61 @@
 package com.example.carlosnieto.apppagos;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.carlosnieto.apppagos.Fragments.FragmentListaClientes;
+import com.example.carlosnieto.apppagos.Fragments.FragmentMenu;
 import com.example.carlosnieto.apppagos.Fragments.FragmentRegistraClienteNuevo;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements FragmentMenu.OnFragmentInteractionListener,
+        FragmentRegistraClienteNuevo.OnFragmentInteractionListener,
+        FragmentListaClientes.OnFragmentInteractionListener {
 
-    Button bt_guarda_cliente;
+    FragmentMenu fragmentMenu;
+    FragmentRegistraClienteNuevo fragmentRegistraClienteNuevo;
+    FragmentListaClientes fragmentListaClientes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        ConexionSQliteHelper conn=new ConexionSQliteHelper(this,"bd_usuarios", null,1);
+        fragmentMenu=new FragmentMenu();
+        fragmentRegistraClienteNuevo=new FragmentRegistraClienteNuevo();
+        fragmentListaClientes=new FragmentListaClientes();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragment,fragmentMenu).commit();
+
+        //ConexionSQliteHelper conn=new ConexionSQliteHelper(this,"bd_usuarios", null,1);
     }
 
-    public void nuevo_cliente (View view)
+    public void onClick (View view)
     {
-        Intent obj  = new Intent(MenuActivity.this,FragmentRegistraClienteNuevo.class);
-        startActivity(obj);
+        android.support.v4.app.FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+
+            switch (view.getId()){
+                case R.id.btnNuevoCliente:
+                    break;
+                case R.id.btnEstadisticas:
+                    break;
+                case R.id.btnListaClientes:
+                    break;
+                case R.id.btnPrestamo:
+            }
+        transaction.commit();
     }
 
-    public void ver_creditos (View view)
-    {
-        Intent credit  = new Intent(MenuActivity.this,ConsultarClientesActivity.class);
-        startActivity(credit);
-    }
 
-    public void nuevo_prestamo (View view)
-    {
-        Intent credit  = new Intent(MenuActivity.this,NuevoPrestamo.class);
-        startActivity(credit);
-    }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
-    public void ver_ganancias(View view) {
-        Intent credit  = new Intent(MenuActivity.this,ConsultarCreditos.class);
-        startActivity(credit);
     }
 
     //Falta el de consultar creditos para poner en el menu
